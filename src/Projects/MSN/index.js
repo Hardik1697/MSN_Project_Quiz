5e29c90dd0d3ce8027f337d459e6e300e0c0eed4
Hardik Anand <hanand@email.sc.edu>
2020-07-28 02:46:47 -0400

    Added more functionality to the code

--- src/Projects/MSN/index.js
+++ src/Projects/MSN/index.js
@@ -34,22 +34,22 @@ function validate() {
     if (q1_bool == true) {
         document.getElementById("q1_check").innerHTML = "Question 1: Correct";
     } else {
-        document.getElementById("q1_check").innerHTML = "Question 1: Incorrect, correct choice is: Choice#1";
+        document.getElementById("q1_check").innerHTML = "Question 1: Incorrect, correct choice is: 3";
     }
     if (q2_bool == true) {
         document.getElementById("q2_check").innerHTML = "Question 2: Correct";
     } else {
-        document.getElementById("q2_check").innerHTML = "Question 2: Incorrect, correct choice is: Choice#1";
+        document.getElementById("q2_check").innerHTML = "Question 2: Incorrect, correct choice is: AI used in vacuum cleaners";
     }
     if (q3_bool == true) {
         document.getElementById("q3_check").innerHTML = "Question 3: Correct";
     } else {
-        document.getElementById("q3_check").innerHTML = "Question 3: Incorrect, correct choice is: Choice#1";
+        document.getElementById("q3_check").innerHTML = "Question 3: Incorrect, correct choice is: Robert De Niro";
     }
     if (q4_bool == true) {
         document.getElementById("q4_check").innerHTML = "Question 4: Correct";
     } else {
-        document.getElementById("q4_check").innerHTML = "Question 4: Incorrect, correct choice is: Choice#1";
+        document.getElementById("q4_check").innerHTML = "Question 4: Incorrect, correct choice is: Yes";
     }
     document.getElementById("tot_score").innerHTML = "Total score: " + score;
     submitted = true
@@ -64,10 +64,16 @@ function submitted_or_not() {
 }
 
 function next_click() {
-    var x = document.getElementById("article_div");
-    x.style.display = "none";
-    var y = document.getElementById("quiz_div");
-    if (y.style.visibility = "hidden") {
-        y.style.visibility = "visible";
+    var choice = false
+    if (confirm("Proceeding further will not let you go back Do you wish to continue?")) {
+        choice = true;
+    }
+    if (choice == true) {
+        var x = document.getElementById("article_div");
+        x.style.display = "none";
+        var y = document.getElementById("quiz_div");
+        if (y.style.visibility = "hidden") {
+            y.style.visibility = "visible";
+        }
     }
 }
